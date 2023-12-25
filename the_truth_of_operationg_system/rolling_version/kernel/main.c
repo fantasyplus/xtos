874b642cc6ee7fdf57a41f7223d46adb68ea74b1
xt <530925206@qq.com>
2023-12-25 00:18:28 -0800

    添加了最基础的str处理函数库

--- the_truth_of_operationg_system/rolling_version/kernel/main.c
+++ the_truth_of_operationg_system/rolling_version/kernel/main.c
@@ -1,11 +1,20 @@
 #include "print.h"
 #include "init.h"
 #include "debug/debug.h"
+#include "../lib/string.h"
 
-void main(void) {
+int main(void)
+{
    put_str("I am kernel\n");
    init_all();
-   ASSERT(123123<0);
+   // ASSERT(123123<0);
+   char *a = "123";
+   char *b = "abc";
+   strcat(a, b);
+   put_str(a);
 
-   while(1);
+   while (1)
+      ;
+
+   return 0;
 }
