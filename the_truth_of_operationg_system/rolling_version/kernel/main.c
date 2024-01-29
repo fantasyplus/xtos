a89b246e80c521960c5179eebcae61618dd65f76
xt <530925206@qq.com>
2024-01-28 22:43:49 -0800

    一些小改动

--- the_truth_of_operationg_system/rolling_version/kernel/main.c
+++ the_truth_of_operationg_system/rolling_version/kernel/main.c
@@ -9,7 +9,7 @@ int main(void)
    put_str("I am kernel\n");
    init_all();
 
-   void* addr = get_kernel_pages(3);
+   void* addr = get_kernel_pages(100);
    put_str("\n get_kernel_page start vaddr is ");
    put_int((uint32_t)addr);
    put_str("\n");
