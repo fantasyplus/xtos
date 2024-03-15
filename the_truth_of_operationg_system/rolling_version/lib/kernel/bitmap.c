8beeb122352f89b6710a19a04e8ce06f2f57f09e
fantasyplus <530925206@qq.com>
2024-03-15 18:59:19 +0800

    little fix

--- the_truth_of_operationg_system/rolling_version/lib/kernel/bitmap.c
+++ the_truth_of_operationg_system/rolling_version/lib/kernel/bitmap.c
@@ -31,7 +31,9 @@ int bitmap_scan(struct bitmap *bitmap, uint32_t cnt)
             area_start++;
         }
         if (area_start / 8 >= bitmap->btmp_bytes_len) // 上面那个循环跑完可能是area_start已经越过边界，说明此时位图中是全1，那么就没有可分配内存
+        {
             return -1;
+        }
 
         area_size = 1; // 来到了这一句说明找到了位图中第一个0，那么此时area_size自然就是1
         while (area_size < cnt)
@@ -43,16 +45,20 @@ int bitmap_scan(struct bitmap *bitmap, uint32_t cnt)
             }
 
             if (bitmap_scan_test(bitmap, area_start + area_size) == 0) // 判断区域起始0的下一位是否是0
+            {
                 area_size++;
+            }
             else
+            {
                 break; // 进入else，说明下一位是1，此时area_size还没有到达cnt的要求，且一片连续为0的区域截止，break
+            }
         }
         if (area_size == cnt) // 有两种情况另上面的while结束，1、area_size == cnt；2、break；所以要判断
         {
             return area_start;
         }
 
-        //如果当前area_start开始的地方不满足
+        // 如果当前area_start开始的地方不满足
         area_start += (area_size + 1); // 更新area_start，判断后面是否有满足条件的连续0区域
     }
 }
